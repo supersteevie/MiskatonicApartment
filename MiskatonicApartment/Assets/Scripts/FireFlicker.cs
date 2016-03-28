b282cf4db4550b4c20dc34356d942c74e07dde8c
Stephen Larsen <Stephen Larsen>
2016-03-28 00:47:35 -0600

    Made a pretty schweet fire texture in substance. Spent an hour trying to make a texture sheet of it but no luck.

---
+++ MiskatonicApartment/Assets/Scripts/FireFlicker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireFlicker : MonoBehaviour {
+
+    private ParticleSystem flame;
+    private Light flicker;
+    public float flkrAmt;
+
+    // Use this for initialization
+    void Start () {
+        flicker = gameObject.GetComponent<Light>();
+        flkrAmt = 2.2f;
+        flame = gameObject.GetComponent<ParticleSystem>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        flicker.range = flame.particleCount / flkrAmt;
+
+        
+	}
+}
